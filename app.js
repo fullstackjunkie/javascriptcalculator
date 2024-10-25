06062f0046b1e013e6160986cd131142e1badc37
fullstackjunkie <dirtroadddeveloper@gmail.com>
2024-10-25 18:52:35 -0400

    Finished Calculator

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const display = document.querySelector(".display");
 const numbers = document.querySelector(".numbers");
-let numbersOne = [];
+const operators = document.querySelector(".operators");
 
+let numbersOne = [];
+let numbersTwo = [];
+let numTwo = "";
+let mOperator = "";
+let numMain = "";
 display.textContent = 0;
 
 function add(num1, num2) {
@@ -46,16 +51,69 @@ function operate(operator, num1, num2) {
 numbers.addEventListener("click", (e) => {
     // display.textContent = e.target.textContent;
     numbersOne.push(e.target.textContent);
-    console.log(numbersOne);
-    displayContent(numbersOne);
+    
+    let numOne = getNum(numbersOne);
+    
+    let numTwo = "";
+    let operation = e.target.textContent;
+   
+    display.textContent = numOne;
+    
+    if(operation == "+") {
+        
+        
+        mOperator = "+";
+        
+        display.textContent = "0";
+        numMain = numOne;
+        numbersOne = [];
+    }
+    if(operation == "-") {
+        mOperator = "-";
+        display.textContent = "0";
+        numMain = numOne;
+        numbersOne = [];
+    }
+    if(operation == "*") {
+        mOperator = "*";
+        display.textContent = "0";
+        numMain = numOne;
+        numbersOne = [];
+    }
+    if(operation == "/") {
+        mOperator = "/";
+        display.textContent = "0";
+        numMain = numOne;
+        numbersOne = [];
+    }
+    if(operation == "AC") {
+        mOperator = "";
+        display.textContent = "0";
+        numMain = "";
+        numbersOne = [];
+    }
+
+    if(operation == "=") {
+        let finNum = numMain.slice(0, numMain.length - 1);
+        let secFinNum = numOne.slice(0, numOne.length - 1);
+        console.log(`mOperator is ${mOperator}`);
+        console.log(`numMain is ${finNum}`);
+        console.log(`numOne is ${secFinNum}`);
+        display.textContent = operate(mOperator, Number(finNum), Number(secFinNum));
+    }
 });
 
 
 
-function displayContent(arr) {
+function getNum(arr) {
     let num = "";
     for(let i = 0; i < arr.length; i++) {
-         num += arr[i]
+        
+            num += arr[i]
+        
+            
+        
+         
     }
-    display.textContent = num;
+    return num;
 }
